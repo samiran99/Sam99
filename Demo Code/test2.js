7b99e95847f4d1764cc481fa0d663f974283091a
Sam99 <babusarkar1999@gmail.com>
2023-07-02 23:19:05 +0530

    Modified test2

--- Demo Code/test2.js
+++ Demo Code/test2.js
@@ -1,1 +1,9 @@
-console.log('2')
+var i=0;
+var p = [];
+for(j=0;j<=100;j++){
+    i=i+1;
+    if(i%2==0 && i%3==0){
+        p.push(i);
+    }
+}
+console.log(p.sort(function(a, b){return a-b})[3]);
